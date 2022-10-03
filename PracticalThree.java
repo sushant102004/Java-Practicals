2cfc5d0be4a39c66e46086aa1c9d476be1b50ae9
sushant102004 <sushant.dhiman9812@gmail.com>
2022-10-03 22:23:06 +0530

    Practical Three Completed

--- PracticalThree.java
+++ PracticalThree.java
@@ -2,18 +2,40 @@ import java.util.Scanner;
 
 class PracticalThree {
     public static void main(String[] args){
+        double tax = 0;
 
-        // Scanner sc = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
-        // System.out.print("Enter Category: ");
-        // String category = sc.nextLine();
-
-        // System.out.print("Enter Total Income: ");
-        // int income = sc.nextInt();
-
-        // System.out.println(category);
-
+        System.out.print("Enter Your Category: ");
+        String category = scanner.nextLine();
         
-        
+        System.out.print("Enter Your Income: ");
+        int income = scanner.nextInt();
+
+        if(category.equals("General")){
+            if(income <= 180000){
+                System.out.println("Tax : " + tax);
+                return;
+            } else if(income <= 500000){
+                tax = 180000 * 0 + (income - 180000) * 0.10;
+                System.out.println("Tax : " + tax);
+            } else if(income <= 800000){
+                tax = 180000 * 0 + 500000 * 0.10 + (income - 500000) * 0.20;
+            } else {
+                tax = 180000 * 0 + 500000 * 0.10 + 800000 * 0.20 + (income - 800000) * 0.30;
+        }
+    } else if(category.equals("Women")){
+            if(income <= 190000){
+                System.out.println("Tax : " + tax);
+                return;
+            } else if(income <= 500000){
+                tax = 180000 * 0 + (income - 180000) * 0.10;
+                System.out.println("Tax : " + tax);
+            } else if(income <= 800000){
+                tax = 180000 * 0 + 500000 * 0.10 + (income - 500000) * 0.20;
+            } else {
+                tax = 180000 * 0 + 500000 * 0.10 + 800000 * 0.20 + (income - 800000) * 0.30;
+        }
     }
 }
+} 
